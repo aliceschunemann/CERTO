ced7cdcb765d39326a261ec30cd3c67cd4ede8db
Alice Schünemann <alice.schunemann.cel@gmail.com>
2023-08-16 17:43:44 -0300

    i

--- AAAAAAAAAAAAAAAa/Startup.cs
+++ AAAAAAAAAAAAAAAa/Startup.cs
@@ -53,6 +53,9 @@ namespace AAAAAAAAAAAAAAAa
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                     name: "areaRoute",
+                     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
