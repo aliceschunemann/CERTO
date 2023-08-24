29d39a3095194268d95ff885f9e392598fa4c9d3
Alice Schünemann <alice.schunemann.cel@gmail.com>
2023-08-24 17:51:28 -0300

    Comecei a arrumar o design

--- AAAAAAAAAAAAAAAa/Data/DAL/Docente/ProfessorDAL.cs
+++ AAAAAAAAAAAAAAAa/Data/DAL/Docente/ProfessorDAL.cs
@@ -1,11 +1,20 @@
-﻿namespace AAAAAAAAAAAAAAAa.Data.DAL.Docente
+﻿using Modelo.Docente;
+using System.Linq;
+
+namespace AAAAAAAAAAAAAAAa.Data.DAL.Docente
 {
     public class ProfessorDAL
     {
         private IESContext _context;
+
         public ProfessorDAL(IESContext context)
         {
             _context = context;
+        }
+
+        public IQueryable<Professor> ObterProfessoresClassificadosPorNome()
+        {
+            return _context.Professores.OrderBy(b => b.Nome);
         }
     }
 }
--- AAAAAAAAAAAAAAAa/Models/Infra/AcessarViewModel.cs
+++ AAAAAAAAAAAAAAAa/Models/Infra/AcessarViewModel.cs
@@ -5,12 +5,14 @@ namespace AAAAAAAAAAAAAAAa.Models.Infra
 {
     public class AcessarViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Preencha esse campo!")]
         [EmailAddress]
         public string Email { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Preencha esse campo!")]
         [DataType(DataType.Password)]
         public string Senha { get; set; }
+
         [Display(Name = "Lembrar de mim?")]
         public bool LembrarDeMim { get; set; }
     }
